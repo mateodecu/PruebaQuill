bd752501932a4cda921bb75d4fcfdedbfb6d7b80
Juan Mateo Decurgez <mateodecu@gmail.com>
2021-02-12 13:41:20 +0100

    Se ha creado el proyecto Angular 7 con nx-quill. Se incorporó un editor quill en la pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PruebaQuill';
+
+  public quillConfig = {
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ color: [] }, { background: [] }],
+        [{ font: [] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ align: [] }],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['blockquote', 'code-block'],
+        ['link', 'emoji'],
+      ],
+    },
+    'emoji-toolbar': true,
+    'emoji-textarea': false,
+    'emoji-shortname': true,
+  };
+
 }
