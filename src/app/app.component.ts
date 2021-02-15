74dd132ee90a6afcdcfa365d7dcba87f96a81a69
Juan Mateo Decurgez <mateodecu@gmail.com>
2021-02-15 18:25:28 +0100

    Mention implementada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill'
+import "quill-mention";
+
+const atValues = [
+  { id: '{{ user.a }}', value: "Todos los alumnos" },
+  { id: '{{ Determinado alumno 1 }}', value: "Determinado alumno 1" },
+  { id: '{{ Determinado alumno 2 }}', value: "Determinado alumno 2" },
+  { id: '{{ Determinado alumno 3 }}', value: "Determinado alumno 3" }
+];
+const hashValues = [
+  { id: '{{ Valor hash 1 }}', value: "Valor hash 1" },
+  { id: '{{ Valor hash 2 }}', value: "Valor hash 2" }
+]
 
 @Component({
   selector: 'app-root',
@@ -21,9 +34,36 @@ export class AppComponent {
         ['link', 'emoji'],
       ],
     },
-    'emoji-toolbar': true,
-    'emoji-textarea': false,
-    'emoji-shortname': true,
+    mention: {
+      allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+      mentionDenotationChars: ["@", "#"],
+      source: function (searchTerm, renderList, mentionChar) {
+        let values;
+
+        if (mentionChar === "@") {
+          values = atValues;
+        } else {
+          values = hashValues;
+        }
+
+        if (searchTerm.length === 0) {
+          renderList(values, searchTerm);
+        } else {
+          const matches = [];
+          for (let i = 0; i < values.length; i++)
+            if (
+              ~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase())
+            )
+              matches.push(values[i]);
+          renderList(matches, searchTerm);
+        }
+      }
+    }
   };
 
+  changedEditor(event: EditorChangeContent | EditorChangeSelection) {
+    // console.log(event)
+
+    // console.log(event.editor.container.innerText);
+  }
 }
