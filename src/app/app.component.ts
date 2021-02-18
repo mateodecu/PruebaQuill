4126ae32881a20032d54217e522f0ba9d6b81fb3
Juan Mateo Decurgez <mateodecu@gmail.com>
2021-02-18 15:37:34 +0100

    Proyecto terminado. Se ha agregado la funcionalidad de repoblar el editor metiante el HTML recibido de la API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,44 @@
-import { Component } from '@angular/core';
-import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill'
+import { Component } from "@angular/core";
+import { EditorChangeContent, EditorChangeSelection } from "ngx-quill";
 import "quill-mention";
+import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 
 const atValues = [
-  { id: '{{ user.a }}', value: "Todos los alumnos" },
-  { id: '{{ Determinado alumno 1 }}', value: "Determinado alumno 1" },
-  { id: '{{ Determinado alumno 2 }}', value: "Determinado alumno 2" },
-  { id: '{{ Determinado alumno 3 }}', value: "Determinado alumno 3" }
+  { id: "{{ user.a }}", value: "Todos los alumnos", blotHTML: `<span class="mention" data-index="0" data-denotation-char="@" data-id="{{ user.a }}" data-value="Todos los alumnos"><span contenteditable="false"><span class="ql-mention-denotation-char">@</span>Todos los alumnos</span></span>` },
+  { id: "{{ Determinado alumno 1 }}", value: "Determinado alumno 1", blotHTML: `<span class="mention" data-index="1" data-denotation-char="@" data-id="{{ Determinado alumno 1 }}" data-value="Determinado alumno 1"><span contenteditable="false"><span class="ql-mention-denotation-char">@</span>Determinado alumno 1</span></span>` },
+  { id: "{{ Determinado alumno 2 }}", value: "Determinado alumno 2", blotHTML: `<span class="mention" data-index="2" data-denotation-char="@" data-id="{{ Determinado alumno 2 }}" data-value="Determinado alumno 2"><span contenteditable="false"><span class="ql-mention-denotation-char">@</span>Determinado alumno 2</span></span>` },
+  { id: "{{ Determinado alumno 3 }}", value: "Determinado alumno 3", blotHTML: `<span class="mention" data-index="3" data-denotation-char="@" data-id="{{ Determinado alumno 3 }}" data-value="Determinado alumno 3"><span contenteditable="false"><span class="ql-mention-denotation-char">@</span>Determinado alumno 3</span></span>` }
 ];
+
 const hashValues = [
-  { id: '{{ Valor hash 1 }}', value: "Valor hash 1" },
-  { id: '{{ Valor hash 2 }}', value: "Valor hash 2" }
-]
+  { id: "{{ Valor hash 1 }}", value: "Valor hash 1" },
+  { id: "{{ Valor hash 2 }}", value: "Valor hash 2" },
+];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'PruebaQuill';
+  title = "Prueba Quill";
+  toSave = null;
+  content = null;
+  ops = null;
+
+  variable = "Hola a todosss"
 
   public quillConfig = {
     toolbar: {
       container: [
-        ['bold', 'italic', 'underline', 'strike'],
+        ["bold", "italic", "underline", "strike"],
         [{ color: [] }, { background: [] }],
         [{ font: [] }],
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
         [{ align: [] }],
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        ['blockquote', 'code-block'],
-        ['link', 'emoji'],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["blockquote", "code-block"],
+        ["link", "emoji"],
       ],
     },
     mention: {
@@ -57,13 +64,32 @@ export class AppComponent {
               matches.push(values[i]);
           renderList(matches, searchTerm);
         }
-      }
-    }
+      },
+    },
   };
 
   changedEditor(event: EditorChangeContent | EditorChangeSelection) {
-    // console.log(event)
+    const contents = event.editor.getContents();
+    this.ops = contents;
+  }
 
-    // console.log(event.editor.container.innerText);
+  convert() {
+    var cfg = {};
+    var converter = new QuillDeltaToHtmlConverter(this.ops.ops, cfg);
+    converter.renderCustomWith((customOp) => {
+      if (customOp.insert.type === "mention") {
+        const mention = customOp.insert.value;
+        return `${mention.id}`;
+      }
+    });
+    var html = converter.convert();
+    this.toSave = html;
+  }
+
+  boton() {
+    let string = this.toSave
+    atValues.forEach((valor) => string = string.replace(RegExp(valor.id, "g"), valor.blotHTML))
+    string = string.replace(RegExp('<br/>', "g"), '</p><p>')
+    document.getElementById("description2").children[1].children[0].innerHTML = string
   }
 }
